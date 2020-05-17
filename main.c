f52106b587cf9d03a0bb250e7513c0b1af7c6b71
maxisuppes <msuppes@eryx.co>
2020-05-17 12:05:31 -0300

    Basic version:
    - Defined matrix initial state taking arguments from console

--- main.c
+++ main.c
@@ -1,6 +1,41 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
     printf("Hello, World!\n");
+
+    // reading arguments
+    int iterations = atoi(argv[1]);
+    int matrix_rows = atoi(argv[2]);
+    int matrix_cols = atoi(argv[3]);
+    char *file_name = argv[4];
+
+    // defining matrix
+    char matrix[matrix_rows][matrix_cols];
+    memset(matrix, 'o', sizeof matrix);
+
+    // fulling matrix with initial state from configuration file
+    FILE *file_pointer = fopen(file_name, "r");
+    char line_of_file[256];
+
+    while (fgets(line_of_file, sizeof(line_of_file), file_pointer)) {
+        char *rest;
+        int row = (int) strtol(line_of_file, &rest, 10);
+        int column = (int) strtol(rest, &rest, 10);
+
+        matrix[row][column] = '-';
+    }
+
+    fclose(file_pointer);
+
+    // printing game initial state
+    for (int i = 0; i < matrix_rows; i++) {
+        for (int j = 0; j < matrix_cols; j++) {
+            printf("%c", matrix[i][j]);
+            if (j == matrix_cols - 1) printf("\n");
+        }
+    }
+
     return 0;
 }
